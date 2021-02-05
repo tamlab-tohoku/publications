07a2ca84cd906d4fbf794e1563a981de3460919a
Yusuke Tamura <tamura@tamlab.jp>
2021-02-05 17:04:26 +0900

    複数JSONファイルを読み込む部分の修正

--- pubtest.js
+++ pubtest.js
@@ -4,19 +4,19 @@
   url2021 = rep_url + "journal_2021.json";
   url2020 = rep_url + "journal_2020.json";
 
-  $.when(
+$.when(
       $.getJSON(url2021),
       $.getJSON(url2020)
-  ).then((data2021, data2020) => {
-      const cdata = data2021.concat(data2020);
-      $("#journal_list").append("<li>" + cdata.length+"<\/li>");
-
-      for(var i in cdata){
-          var authors = cdata[i].author.join(', ');
-          $("#journal_list").append("<li>[" + (cdata.length-i) +"] " + authors + ": <strong>" + cdata[i].title +"<\/strong>, <i>" + cdata[i].journal + "<\/i>, " + cdata[i].vol + ", " + cdata[i].no + ", " + cdata[i].page + ", " + cdata[i].year + ". <a href=\"" +cdata[i].url + "\">" + cdata[i].url + "<\/a>");
-      }
-    }, () => {
-        $("#journal_list").append("<li>Cannot get data.<\/li>")
-    });
+)
+.done(function(data2021, data2020){
+   	const cdata = [...data2021[0], ...data2020[0]];
+   	for(var i in cdata){
+      	var authors = cdata[i].author.join(', ');
+       	$("#journal_list").append("<li>[" + (cdata.length-i) +"] " + authors + ": <strong>" + cdata[i].title +"<\/strong>, <i>" + cdata[i].journal + "<\/i>, " + cdata[i].vol + ", " + cdata[i].no + ", " + cdata[i].page + ", " + cdata[i].year + ". <a href=\"" +cdata[i].url + "\">" + cdata[i].url + "<\/a>");
+    }
+})
+.fail(function(){
+    $("#journal_list").append("<li>Cannot get data.<\/li>")
+});
   
 </script>
