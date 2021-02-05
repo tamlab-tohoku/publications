9ca6d2d0abcf29355f0b79e7cf4babb57751cd59
Yusuke Tamura <tamura@tamlab.jp>
2021-02-05 15:50:33 +0900

    複数のjsonファイルに対応するように変更

--- pubtest.js
+++ pubtest.js
@@ -1,18 +1,22 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script language="javascript" type="text/javascript">
   rep_url = "https://raw.githubusercontent.com/tamlab-tohoku/publications/main/";
-  
-  
-  
-  
-  url = rep_url + "journal_2021.json";
-  $(function() {
-      $.getJSON(url, function(data) {
-             for(var i in data){
-               var authors = data[i].author.join(', ');
-                 $("#journal_list").append("<li>[" + (data.length-i) +"] " + authors + ": <strong>" + data[i].title +"<\/strong>, <i>" + data[i].book + "<\/i>, " + data[i].vol + ", " + data[i].no + ", " + data[i].page + ", " + data[i].year + ". <a href=\"" + data[i].url + "\">" + data[i].url + "<\/a>");
-             }
-      });
-  });
+
+  url2021 = rep_url + "journal_2021.json";
+  url2020 = rep_url + "journal_2020.json";
+
+  $.when(
+      $.getJSON(url2021),
+      $.getJSON(url2020)
+  ).then((data2021, data2020) => {
+      const cdata = [data2021, data2020];
+
+      for(var i in data){
+          var authors = data[i].author.join(', ');
+          $("#journal_list").append("<li>[" + (data.length-i) +"] " + authors + ": <strong>" + data[i].title +"<\/strong>, <i>" + data[i].book + "<\/i>, " + data[i].vol + ", " + data[i].no + ", " + data[i].page + ", " + data[i].year + ". <a href=\"" + data[i].url + "\">" + data[i].url + "<\/a>");
+      }
+    }, () => {
+        $("#journal_list").append("<li>Cannot acquire data.<\/li>")
+    });
   
 </script>
