aec2a1261c7be5a62c4bc27052e5c15474428534
Yusuke Tamura <tamura@tamlab.jp>
2021-02-26 15:43:09 +0900

    modified jconf.js

--- jconf.js
+++ jconf.js
@@ -46,14 +46,15 @@ $.when(
 //.done(function(data2021, data2020, data2019, data2018, data2017, data2016, data2015, data2014, data2013, data2012, data2011, data2010, data2009, data2008, data2007, data2006, data2005, data2004, data2003){
 .done(function(data2021){
 //     const cdata = [...data2021[0], ...data2020[0], ...data2019[0], ...data2018[0], ...data2017[0], ...data2016[0], ...data2015[0], ...data2014[0], ...data2013[0], ...data2012[0], ...data2011[0], ...data2010[0], ...data2009[0], ...data2008[0], ...data2007[0], ...data2006[0], ...data2005[0], ...data2004[0], ...data2003[0]];
-  const cdata = [...data2021[0]];
+  const cdata = data2021;
   for(var i in cdata){
     var authors = cdata[i].author.join(', ');
     //authors = authors.replace('Yusuke Tamura', '<u>Yusuke Tamura</u>');
     //authors = authors.replace('田村雄介', '<u>田村雄介</u>');
-    $("#jconf_list").append("<li>" + authors + ": <strong>" + cdata[i].title +"<\/strong>, <i>" + cdata[i].procs + "<\/i>, " + cdata[i].page + ", " + cdata[i].place + ", " + cdata[i].year + "年" + cdata[i].month + "月.");
     if(cdata[i].review != ""){
-      $("#jconf_list").append("("+ cdata[i].review + ")");
+      $("#jconf_list").append("<li>" + authors + ": <strong>" + cdata[i].title +　"<\/strong>, <i>" + cdata[i].procs + "<\/i>, " + cdata[i].page + ", " + cdata[i].place + ", " + cdata[i].year + "年" + cdata[i].month + "月. (" + cdata[i].review + ")");
+    }else{
+      $("#jconf_list").append("<li>" + authors + ": <strong>" + cdata[i].title +　"<\/strong>, <i>" + cdata[i].procs + "<\/i>, " + cdata[i].page + ", " + cdata[i].place + ", " + cdata[i].year + "年" + cdata[i].month + "月.");
     }
     if(cdata[i].award != ""){
       $("#jconf_list").append("<div id=\"award\">" + cdata[i].award + "<\/div>");
