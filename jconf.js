2bac5ca8f039a1b677f81407a8a5b706f26eae40
Yusuke Tamura <tamura@tamlab.jp>
2021-02-26 16:24:54 +0900

    modified jconf.js

--- jconf.js
+++ jconf.js
@@ -23,7 +23,7 @@ url2004 = rep_url + "jconf_2004.json"+"?" + time;
 url2003 = rep_url + "jconf_2003.json"+"?" + time;
 
 $.when(
-    $.getJSON(url2021)/*,
+    $.getJSON(url2021),
     $.getJSON(url2020),
     $.getJSON(url2019),
     $.getJSON(url2018),
@@ -43,10 +43,8 @@ $.when(
     $.getJSON(url2004),
     $.getJSON(url2003)*/
 )
-//.done(function(data2021, data2020, data2019, data2018, data2017, data2016, data2015, data2014, data2013, data2012, data2011, data2010, data2009, data2008, data2007, data2006, data2005, data2004, data2003){
-.done(function(data2021){
-//     const cdata = [...data2021[0], ...data2020[0], ...data2019[0], ...data2018[0], ...data2017[0], ...data2016[0], ...data2015[0], ...data2014[0], ...data2013[0], ...data2012[0], ...data2011[0], ...data2010[0], ...data2009[0], ...data2008[0], ...data2007[0], ...data2006[0], ...data2005[0], ...data2004[0], ...data2003[0]];
-  const cdata = data2021;
+.done(function(data2021, data2020, data2019, data2018, data2017, data2016, data2015, data2014, data2013, data2012, data2011, data2010, data2009, data2008, data2007, data2006, data2005, data2004, data2003){
+  const cdata = [...data2021[0], ...data2020[0], ...data2019[0], ...data2018[0], ...data2017[0], ...data2016[0], ...data2015[0], ...data2014[0], ...data2013[0], ...data2012[0], ...data2011[0], ...data2010[0], ...data2009[0], ...data2008[0], ...data2007[0], ...data2006[0], ...data2005[0], ...data2004[0], ...data2003[0]];
   for(var i in cdata){
     var authors = cdata[i].author.join(', ');
     //authors = authors.replace('Yusuke Tamura', '<u>Yusuke Tamura</u>');
